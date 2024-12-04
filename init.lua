aa558a756d88dd6644459337efc34cfeb49c8184
Syiin <xbio.oo@gmail.com>
2024-12-05 00:05:54 +0800

    add ruby stuff

--- init.lua
+++ init.lua
@@ -611,6 +611,8 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         dart = {},
+        ruby_lsp = {},
+        rubyfmt = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
